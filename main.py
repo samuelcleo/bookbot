4f2cee6871ab3ddb2a9fd75fa6470d0e024ee360
samuelcleo <cleophassamuel@gmail.com>
2025-07-30 09:41:19 +1000

    added sys.argv cli commands to consume multiple files

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
+import sys
 from stats import get_book_text
-def main():
-    book_text = get_book_text("books/frankenstein.txt")
-    words_list = book_text.split()
-    print(f"{len(words_list)} words found in the document")
+from stats import count_characters
 
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+else:
+    book_path = sys.argv[1]
+
+def main():
+    book_text = get_book_text(book_path)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+
+    words_list = book_text.split()
+    print(f"Found {len(words_list)} total words")
+
+    print("--------- Character Count -------")
+    count_dict = count_characters(book_text)
+    def sort_on(dict):
+        return dict['count']
+
+    char_list = []
+    for char in count_dict:
+        if char.isalpha():
+            char_list.append({'letter': char, 'count': count_dict[char]})
+    char_list.sort(reverse=True, key=sort_on)
+    for char_dict in char_list:
+        print(f"{char_dict['letter']}: {char_dict['count']}")
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,18 @@ def get_book_text(filepath):
     with open(filepath) as f:
         filecontents = f.read()
     return filecontents
+
+def count_characters(text):
+    char_count = {}
+
+    # Convert text to lowercase to avoid duplicates
+    text = text.lower()
+
+    # Count each character
+    for char in text:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+
+    return char_count
